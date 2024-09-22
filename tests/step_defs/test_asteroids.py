1640c8eff99c5f5576f3e9ce022ba29babf9c5eb
James Walters <waltersjameswalters@gmail.com>
2024-09-22 19:03:49 +0100

    Add allure-report folder to .gitignore, attach JSON data to allure report to be output

--- tests/step_defs/test_asteroids.py
+++ tests/step_defs/test_asteroids.py
@@ -1,4 +1,5 @@
-import pytest
+import allure
+
 from pytest_bdd import scenarios, scenario, given, when, then, parsers
 from make_requests import make_request_feed
 from make_requests import make_request_browse
@@ -25,26 +26,31 @@ def asteroids_response():
 @given("the Asteroids API is queried with no search parameters", target_fixture='asteroids_response')
 def asteroids_response():
     response = make_request_feed(f'api_key={ASTEROIDS_API_KEY}')
+    allure.attach("% s" % response.json(),name="JSON Response", attachment_type=allure.attachment_type.JSON)
     return response
 
 @given(parsers.parse('the Asteroids API is queried with start date "{start_date}"'), target_fixture='asteroids_response')
 def asteroids_response_start_date(start_date):
     response = make_request_feed(f'api_key={ASTEROIDS_API_KEY}&start_date={start_date}')
+    allure.attach("% s" % response.json(),name="JSON Response", attachment_type=allure.attachment_type.JSON)
     return response  
 
 @given(parsers.parse('the Asteroids API is queried with end date "{end_date}"'), target_fixture='asteroids_response')
 def asteroids_response_end_date(end_date):
     response = make_request_feed(f'api_key={ASTEROIDS_API_KEY}&end_date={end_date}')
+    allure.attach("% s" % response.json(),name="JSON Response", attachment_type=allure.attachment_type.JSON)
     return response 
 
 @given(parsers.parse('the Asteroids API is queried with start date "{start_date}" and end date "{end_date}"'), target_fixture='asteroids_response')
 def asteroids_response_date_range(start_date, end_date):
     response = make_request_feed(f'api_key={ASTEROIDS_API_KEY}&start_date={start_date}&end_date={end_date}')
+    allure.attach("% s" % response.json(),name="JSON Response", attachment_type=allure.attachment_type.JSON)
     return response 
 
 @given("the Asteroids API is queried with an invalid token", target_fixture='asteroids_response')
 def asteroids_response_invalid_token():
     response = make_request_feed(f'api_key={INVALID_ASTEROIDS_API_KEY}')
+    allure.attach("% s" % response.json(),name="JSON Response", attachment_type=allure.attachment_type.JSON)
     return response
 
 # Then Steps
@@ -56,30 +62,36 @@ def asteroids_response_code_200(asteroids_response, code):
 def asteriods_response_count(asteroids_response):
     data = asteroids_response.json()
     assert len(data) > 0
-    assert data["element_count"] > 0
+    assert data["element_count"] > 0    
+    allure.attach("% s" % data["element_count"],name="JSON Element Count", attachment_type=allure.attachment_type.JSON)
 
 @then(parsers.parse('the error code is "{code}"'))
 def asteroids_response_error_code(asteroids_response, code):
     data = asteroids_response.json()
     assert data["error"]["code"] == code
+    allure.attach("% s" % data["error"]["code"],name="JSON Error Code", attachment_type=allure.attachment_type.JSON)
 
 @then(parsers.parse('the error message is "{message}"'))
 def asteroids_response_error_message(asteroids_response, message):
     data = asteroids_response.json()
     assert data["error"]["message"] == message
+    allure.attach("% s" % data["error"]["message"],name="JSON Error Message", attachment_type=allure.attachment_type.JSON)
 
 @then(parsers.parse('the http error message is "{http_error_message}"'))
 def asteroids_response_http_error_message(asteroids_response, http_error_message):
     data = asteroids_response.json()
     assert data["error_message"] == http_error_message
+    allure.attach("% s" % datadata["error_message"],name="JSON Error Message", attachment_type=allure.attachment_type.JSON)
 
 @then(parsers.parse('the http error is "{http_error}"'))
 def asteroids_response_http_error(asteroids_response, http_error):
     data = asteroids_response.json()
     assert data["http_error"] == http_error
+    allure.attach("% s" % data["http_error"],name="JSON HTTP ERROR", attachment_type=allure.attachment_type.JSON)
 
 @then(parsers.parse('the element count is between "{element_count_lower:n}" and "{element_count_upper:n}"'))
 def asteroids_response_element_count(asteroids_response, element_count_lower, element_count_upper):
     data = asteroids_response.json()
     assert data["element_count"] > element_count_lower
     assert data["element_count"] < element_count_upper
+    allure.attach("% s" % data["element_count"],name="JSON Element Count", attachment_type=allure.attachment_type.JSON)
