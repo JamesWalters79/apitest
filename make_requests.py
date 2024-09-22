e28e21640d324f2df519a4cf3b06286c84a37f44
James Walters <waltersjameswalters@gmail.com>
2024-09-22 12:46:13 +0100

    Refactoring to use constant for BASE_URL

--- make_requests.py
+++ make_requests.py
@@ -1,16 +1,16 @@
 import requests
 
+#Constants
+BASE_URL = "https://api.nasa.gov/neo/rest/v1/"
+
 def make_request_feed(query_parameters):
-    base_url = "https://api.nasa.gov/neo/rest/v1/feed"
-    response = requests.get(f'{base_url}?{query_parameters}')
+    response = requests.get(f'{BASE_URL}feed?{query_parameters}')
     return response
 
 def make_request_browse(query_parameters):
-    base_url = "https://api.nasa.gov/neo/rest/v1/neo/browse"
-    response = requests.get(f'{base_url}?{query_parameters}')
+    response = requests.get(f'{BASE_URL}neo/browse?{query_parameters}')
     return response
 
 def make_request_lookup(lookup_id,query_parameters):
-    base_url = "https://api.nasa.gov/neo/rest/v1/neo/"
-    response = requests.get(f'{base_url}{lookup_id}?{query_parameters}')
+    response = requests.get(f'{BASE_URL}neo/{lookup_id}?{query_parameters}')
     return response
